d4a3d5ac63a49fca47c90e9bf1d1810e5a491785
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-03 09:27:06 -0300

    Finished header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import styles from "./header.module.css";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.imageContainer}>
+        <Image
+          className={styles.image}
+          src="https://thispersondoesnotexist.com/"
+          fill={true}
+          alt="Profile image"
+        />
+      </div>
+      <div>
+        <h1 className={styles.title}>Martin Pilaszek</h1>
+        <h2 className={styles.subtitle}>Front-end developer</h2>
+      </div>
+    </header>
+  );
 }

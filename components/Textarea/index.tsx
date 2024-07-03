500746674bddeca367a1913a04f49cf5965c5874
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-03 09:39:43 -0300

    Created textarea component

---
+++ components/Textarea/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import styles from "./textarea.module.css";
+
+interface Props {
+  onChange: (value: string, name: string) => any;
+  value: string | number;
+  label?: string;
+  name: string;
+}
+
+export default function Textarea({ onChange, value, label, name }: Props) {
+  const handleChange = (evt: React.FormEvent<HTMLTextAreaElement>) => {
+    onChange(evt.currentTarget.value.toString(), name);
+  };
+
+  return (
+    <div className={styles.inputContainer}>
+      {label && <label htmlFor={name}>{label}</label>}
+      <textarea
+        onChange={handleChange}
+        className={styles.textarea}
+        value={value}
+        id={name}
+      />
+    </div>
+  );
+}

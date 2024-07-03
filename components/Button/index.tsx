65af93240e2866025aa286152ae77197a4297985
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-03 09:30:08 -0300

    Created button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -5,12 +5,25 @@ import styles from "./button.module.css";
 interface Props {
   type?: "button" | "submit" | "reset";
   children?: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
   onClick?: () => any;
 }
 
-export default function Button({ type, onClick, children }: Props) {
+export default function Button({
+  type,
+  disabled,
+  onClick,
+  children,
+  className,
+}: Props) {
   return (
-    <button className={styles.button} onClick={onClick} type={type || "button"}>
+    <button
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+      type={type || "button"}
+    >
       {children}
     </button>
   );

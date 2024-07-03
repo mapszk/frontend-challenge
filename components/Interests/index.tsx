fe78a6b1aa82e3990ba031e8a3608000c5fe9d36
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-03 09:22:23 -0300

    FInished interests section

--- components/Interests/index.tsx
+++ components/Interests/index.tsx
@@ -1,3 +1,18 @@
+import styles from "./interests.module.css";
+
 export default function Interests() {
-  return <section></section>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>Intereses</h1>
+      <div className={styles.grid}>
+        <div className={styles.gridItem}>Cine</div>
+        <div className={styles.gridItem}>Lectura</div>
+        <div className={styles.gridItem}>Tenis</div>
+        <div className={styles.gridItem}>Tecnología</div>
+        <div className={styles.gridItem}>Viajar</div>
+        <div className={styles.gridItem}>Mascotas</div>
+        <div className={styles.gridItem}>Música</div>
+      </div>
+    </section>
+  );
 }

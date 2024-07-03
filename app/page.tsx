a19ccc13e38122db6f408b5b803a6f72e6d0ea4d
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-02 22:00:35 -0300

    Added basic components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import About from "@/components/About";
+import ContactForm from "@/components/ContactForm";
+import Header from "@/components/Header";
+import Interests from "@/components/Interests";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+      <About />
+      <Interests />
+      <ContactForm />
+    </main>
+  );
 }

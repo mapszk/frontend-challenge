079fb3434510215553d9c735ad7ea59294d67d04
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-03 09:22:12 -0300

    Finished about section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,3 +1,18 @@
+import styles from "./about.module.css";
+
 export default function About() {
-  return <section></section>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>Sobre mi</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam amet
+        repellat unde ad ipsa nemo similique iste, assumenda delectus
+        consectetur adipisci soluta incidunt quisquam ratione! Obcaecati dolore
+        maiores quas vitae! Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Error, magnam aliquam! Placeat, quibusdam odit. Vero, nisi nam
+        vitae mollitia sit voluptate perferendis. In corporis aliquid ipsa omnis
+        repellendus quis at.
+      </p>
+    </section>
+  );
 }
